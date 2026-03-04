e314768eaa13ea3c1688a0ebda9b1e10774a67a8
Taryn McHugh <tjm7063@psu.edu>
2026-03-04 16:02:25 -0500

    Add 3 subtabs

--- frontend/DS440W/src/views/LandingView.vue
+++ frontend/DS440W/src/views/LandingView.vue
@@ -1,5 +1,6 @@
 <template>
-  <v-container class="fill-height d-flex flex-column align-center justify-center">
+  <v-container fluid class="fill-height d-flex flex-column">
+    
     
     <h1 class="mb-8">Welcome to Your Recipe App!</h1>
 
@@ -13,9 +14,37 @@
     <!-- Content -->
     <v-window v-model="selectedTab" class="mt-6">
       <v-window-item v-for="(recipe, index) in recipes" :key="index">
-        <v-card width="400" class="pa-4 text-center">
+        <v-card class="pa-4 text-center">
           <h2>{{ recipe }}</h2>
           <p>Here you can add details for {{ recipe }}.</p>
+          
+           <!-- SUB TABS -->
+          <v-tabs
+            v-model="selectedSubTab"
+            color="secondary"
+            align-tabs="center"
+          >
+            <v-tab v-for="tab in subTabs" :key="tab">
+              {{ tab }}
+            </v-tab>
+          </v-tabs>
+
+          <!-- SUB TAB CONTENT -->
+          <v-window v-model="selectedSubTab" class="mt-4">
+            <v-window-item
+              v-for="tab in subTabs"
+              :key="tab"
+            >
+              <div class="text-center pa-4">
+                <h3>{{ tab }}</h3>
+                <p>
+                  Content for <strong>{{ recipe }}</strong> - {{ tab }}
+                </p>
+              </div>
+            </v-window-item>
+          </v-window>
+
+
         </v-card>
       </v-window-item>
     </v-window>
@@ -39,5 +68,8 @@ const recipes = [
   "Veggie Sandwich"
 ]
 
+const subTabs = ["Ingredients", "Nutrition", "Instruction"]
+
 const selectedTab = ref(0)
+const selectedSubTab = ref(0)
 </script>
